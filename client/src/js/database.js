96ffcb1ca06e803f35233097db1065138718f634
Jeanette Ralph <jean.ralph@hotmail.com>
2022-09-04 16:33:45 -0700

    worked on crud operations

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,42 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  console.log('PUT to the database');
+
+  const jateDb = await openDB('content', 1);
+
+  const tx = jateDb.transaction('content', 'readwrite');
+
+  const store = tx.objectStore('content');
+
+  const request = store.put({ id: id, content: text });
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  console.log('GET from the database');
+
+  const jateDb = await openDB('content', 1);
+
+  const tx = jateDb.transaction('content', 'readonly');
+
+  const store = tx.objectStore('content');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+}
+
 
 initdb();
